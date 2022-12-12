223cc872c2f19fcc0a8d745f7cff663110d73a65
jenslycke <jenslycke@gmail.com>
2022-12-12 13:15:42 +0100

    day 11: part 1

--- src/11.ts
+++ src/11.ts
@@ -1,42 +1,51 @@
-import { trim } from 'lodash'
+import { range } from 'lodash'
 import { getDataForDay } from './utils'
 
-// const data = await getDataForDay(11)
+const data = await getDataForDay(11)
 
-const data = `Monkey 0:
-Starting items: 79, 98
-Operation: new = old * 19
-Test: divisible by 23
-  If true: throw to monkey 2
-  If false: throw to monkey 3
+// const data = `Monkey 0:
+// Starting items: 79, 98
+// Operation: new = old * 19
+// Test: divisible by 23
+//   If true: throw to monkey 2
+//   If false: throw to monkey 3
 
-Monkey 1:
-Starting items: 54, 65, 75, 74
-Operation: new = old + 6
-Test: divisible by 19
-  If true: throw to monkey 2
-  If false: throw to monkey 0
+// Monkey 1:
+// Starting items: 54, 65, 75, 74
+// Operation: new = old + 6
+// Test: divisible by 19
+//   If true: throw to monkey 2
+//   If false: throw to monkey 0
 
-Monkey 2:
-Starting items: 79, 60, 97
-Operation: new = old * old
-Test: divisible by 13
-  If true: throw to monkey 1
-  If false: throw to monkey 3
+// Monkey 2:
+// Starting items: 79, 60, 97
+// Operation: new = old * old
+// Test: divisible by 13
+//   If true: throw to monkey 1
+//   If false: throw to monkey 3
 
-Monkey 3:
-Starting items: 74
-Operation: new = old + 3
-Test: divisible by 17
-  If true: throw to monkey 0
-  If false: throw to monkey 1`
+// Monkey 3:
+// Starting items: 74
+// Operation: new = old + 3
+// Test: divisible by 17
+//   If true: throw to monkey 0
+//   If false: throw to monkey 1`
 
 const solveTasks = () => {
   const monkeys = parseMonkeys(data)
 
-  // Do stuff
+  range(0, 20).forEach(round => {
+    monkeys.forEach(monkey => {
+      const numItems = monkey.items.length
+      for (let i = 0; i < numItems; i++) {
+        const { item, throwTo } = monkey.inspectItem()
+        monkeys[throwTo].catchItem(item)
+      }
+    })
+  })
 
-  return monkeys
+  monkeys.sort((a, b) => b.inspections - a.inspections)
+  return monkeys[0].inspections * monkeys[1].inspections
 }
 
 const parseMonkeys = (data: string): Monkey[] => {
@@ -55,20 +64,21 @@ const parseMonkeys = (data: string): Monkey[] => {
       operator
     }
     const test = {
-      test: Number(group[3].trim()),
+      divide: Number(group[3].trim()),
       action: {
         true: Number(group[4].trim()),
         false: Number(group[5].trim())
       }
     }
 
-    return MonkeyFactory.createMonkey(items, operation, test)
+    return MonkeyFactory.create(items, operation, test)
   })
 }
 
 interface Monkey {
   id: number
   items: number[]
+  inspections: number
   operation: Operation
   test: Test
 }
@@ -80,7 +90,7 @@ type Operation = {
 }
 
 type Test = {
-  test: number
+  divide: number
   action: {
     true: number
     false: number
@@ -91,11 +101,60 @@ class Monkey {
   constructor (id, items, operation, test) {
     this.id = id
     this.items = items
+    this.inspections = 0
     this.operation = operation
     this.test = test
   }
 
-  // Do stuff
+  inspectItem () {
+    this.inspections++
+    const item = this.items.shift()
+    console.log(`Monkey ${this.id} inspects item with worry level ${item}`)
+    const inspectedItem = this.updateWorry(item)
+    // console.log(inspectedItem)
+    return this.throwItem(inspectedItem)
+  }
+
+  updateWorry (item): number {
+    const left =
+      this.operation.left === 'old' ? item : Number(this.operation.left)
+    const right =
+      this.operation.right === 'old' ? item : Number(this.operation.right)
+    const operator = this.operation.operator
+
+    if (operator === '+') {
+      item = left + right
+      console.log(`  Worry level increases by ${right} to ${item}.`)
+    } else if (operator === '*') {
+      item = left * right
+      console.log(`  Worry level is multiplied by ${right} to ${item}.`)
+    } else throw new Error('Unkown operator.')
+
+    item = Math.floor(item / 3)
+    console.log(`  Monkey gets bored. Worry level is divided by 3 to ${item}.`)
+    return item
+  }
+
+  throwItem (item: number): { item: number; throwTo: number } {
+    let throwTo: number
+    if (item % this.test.divide === 0) {
+      throwTo = this.test.action.true
+      console.log(`  Current worry level is divisible by ${this.test.divide}.`)
+    } else {
+      throwTo = this.test.action.false
+      console.log(
+        `  Current worry level is not divisible by ${this.test.divide}.`
+      )
+    }
+    console.log(
+      `  Item with worry level ${item} is thrown to monkey ${throwTo}.`
+    )
+    return { item, throwTo }
+  }
+
+  catchItem (item) {
+    this.items.push(item)
+  }
 }
 
 // Seems a bit useless...
@@ -103,11 +162,7 @@ abstract class MonkeyFactory {
   static monkeyCounter: number = 0
   static maxMonkeys: number = 8
 
-  static createMonkey (
-    items: number[],
-    operation: Operation,
-    test: Test
-  ): Monkey {
+  static create (items: number[], operation: Operation, test: Test): Monkey {
     if (this.monkeyCounter >= this.maxMonkeys)
       throw new Error(`Maximum of ${this.maxMonkeys} monkeys already exists.`)
     const monkey = new Monkey(this.monkeyCounter, items, operation, test)
